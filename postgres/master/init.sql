0d6803369c4e8148fe3ccc7b9ab5563dece8630f
Kimsreng <kimsrenghai@gmail.com>
2025-02-04 16:38:48 +0700

    new code

--- postgres/master/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE ROLE replicator WITH REPLICATION PASSWORD 'kim12345' LOGIN;
-ALTER SYSTEM SET wal_level = 'replica';
-ALTER SYSTEM SET max_wal_senders = 10;
-ALTER SYSTEM SET hot_standby = 'on';
-SELECT pg_reload_conf();
